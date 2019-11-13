535b30ee0440bc5a54e0113deca38472064b4928
ruben <alexander.menjivar99@gmail.com>
2019-11-13 13:03:35 -0600

    primer codigo para clase reposit--

---
+++ Documents/NetBeansProjects/ArregloCompaneros2/src/arreglocompaneros2/ArregloCompaneros2.java
@@ -0,0 +1,18 @@
+
+package arreglocompaneros2;
+
+
+public class ArregloCompaneros2 {
+
+   public static void main(String args[])
+    {
+     double[] precios = new double[5];
+     
+     String[] nombre = {"Maynor", "Paola", "Jeffre", "Escarlet", "Aylin"};
+     String[] apellido = {"Martinez", "Reyes", "Quintero", "Castillo", "Maldonado"};
+     String[] carrera = {"Industrial", "Industrial", "Industrial ", "Industrial ", "Industrial"};
+     String[] lugarTrabajo = {"The spot","pepsi", "Gildan ", "Seguro social", "EEH"};
+         
+    }
+
+}
