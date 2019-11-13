6525f418803fce48ea0acc24487b8a7b56fe739e
ruben <alexander.menjivar99@gmail.com>
2019-11-13 12:40:31 -0600

    primer codigo para clase reposit--

---
+++ Documents/NetBeansProjects/Notas/src/notas/Notas.java
@@ -0,0 +1,19 @@
+
+package notas;
+
+public class Notas {
+
+ public static void main(String args[])
+    {
+     String nombre = "Aylin Maldonado";
+     int nota = 89;
+    
+     if (nota < 70)
+     {
+         System.out.println(nombre + " "+ nota + " = Reprobado");
+     } else {
+         System.out.println(nombre + " "+ nota + "= Aprobado");
+     }
+  
+    }
+}   
