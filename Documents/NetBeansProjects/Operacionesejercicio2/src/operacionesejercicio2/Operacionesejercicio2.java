97d1326ec254f6d1697304984e673213d001db15
ruben <alexander.menjivar99@gmail.com>
2019-11-13 13:07:48 -0600

    primer codigo para clase reposit--

---
+++ Documents/NetBeansProjects/Operacionesejercicio2/src/operacionesejercicio2/Operacionesejercicio2.java
@@ -0,0 +1,34 @@
+//Ejercicio2
+package operacionesejercicio2;
+
+public class Operacionesejercicio2 {
+
+    
+    public static void main(String[] args) {
+        int numero1 = 3;
+        int numero2 = 3;
+        int suma = 0;
+        int resta = 0;
+        int multiplicacion = 0;
+        int division = 0;
+        int mod = 0;
+
+       
+
+        suma = numero1 + numero2;
+        resta = numero1 - numero2;
+        multiplicacion = numero1 * numero2;
+        division = numero1 / numero2;
+        mod = numero1 % numero2;
+
+       
+
+        System.out.println("la suma es: " + suma);
+        System.out.println("la resta es: " + resta);
+        System.out.println("la multiplicacion es: " + multiplicacion);
+        System.out.println("la division es: " + division);
+        System.out.println("El mod es: " + mod);
+
+    }
+
+}
