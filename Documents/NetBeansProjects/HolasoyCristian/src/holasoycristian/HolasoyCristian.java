e37818d1d8ff76d49c29872bbe0e5e74fa33bead
ruben <alexander.menjivar99@gmail.com>
2019-11-13 12:39:39 -0600

    primer codigo para clase reposit--

---
+++ Documents/NetBeansProjects/HolasoyCristian/src/holasoycristian/HolasoyCristian.java
@@ -0,0 +1,9 @@
+//Ejercicio1
+package holasoycristian;
+
+public class HolasoyCristian {
+    public static void main(String[] args) {
+       System.out.println("Hola soy Cristian Alexander Menjivar Ayala");
+    }
+    
+}
